8dac9f3f1439fa06d335f4b729828182a13db4b5
user <user@arcadiade-MacBook-Pro.local>
2018-08-06 16:45:27 +0800

    0.0.10

--- lib/microServices/WebSocketManager.d.ts
+++ lib/microServices/WebSocketManager.d.ts
@@ -3,5 +3,7 @@ import * as WebSocket from 'ws';
 export default class WebSocketManager {
     protected _context: WebSocket.Server;
     protected _ows: WebSocket.Server;
+    protected _req: any;
+    protected _head: any;
     constructor();
 }
