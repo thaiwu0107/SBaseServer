ddd331208cfcc3e67f9bd22dc33b7f6e17e567a6
user <user@arcadiade-MacBook-Pro.local>
2018-08-06 19:11:35 +0800

    0.0.13

---
+++ lib/models/OneSocket.d.ts
@@ -0,0 +1,8 @@
+export default class OneSocketContext {
+    private _ows;
+    private _req;
+    private _head;
+    private isAlive;
+    private constructor();
+    private heartbeat;
+}
--- lib/models/WebSocketContext.d.ts
+++ lib/models/WebSocketContext.d.ts
@@ -3,9 +3,7 @@ export default class WebSocketContext {
     private static _instance;
     static getInstance(): WebSocketContext;
     private static _websocket;
-    private static _ows;
-    private static _req;
-    private static _head;
+    private static isAlive;
     private constructor();
     getWebsocket(): WebSocket.Server;
     static init(websocket: any): void;
