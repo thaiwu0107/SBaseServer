14da23809838ca3dafbd633b52f6344c4bc05eb3
user <user@arcadiade-MacBook-Pro.local>
2018-08-06 16:44:56 +0800

    Initial commit of all files to the repository

--- src/microServices/WebSocketManager.ts
+++ src/microServices/WebSocketManager.ts
@@ -7,20 +7,24 @@ const _log = log4js.getLogger('WebSocketManager');
 export default class WebSocketManager {
     protected _context: WebSocket.Server;
     protected _ows: WebSocket.Server;
+    protected _req: any;
+    protected _head: any;
     constructor() {
         this._context = WebSocketContext.getInstance().getWebsocket();
         this._context.on('connection', (ws, req, head) => {
             this._ows = ws;
+            this._req = req;
+            this._head = head;
             console.log(req.connection.remoteAddress);
-        });
-        this._ows.on('open', function open() {
-            console.log('connected');
-        });
-        this._ows.on('close', function close() {
-            console.log('disconnected');
-        });
-        this._ows.on('message', function incoming(data) {
-            console.log(`Roundtrip time: ${Date.now() - data} ms`);
+            ws.on('open', () => {
+                console.log('connected');
+            });
+            ws.on('close', () => {
+                console.log('disconnected');
+            });
+            ws.on('message', (data) => {
+                console.log('message', data);
+            });
         });
     }
 }
