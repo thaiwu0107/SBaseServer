7f08bfe7e23bd6175d8ed8a6ec20e62a92995fe4
user <user@arcadiade-MacBook-Pro.local>
2018-08-06 19:09:03 +0800

    Initial commit of all files to the repository

--- src/models/WebSocketContext.ts
+++ src/models/WebSocketContext.ts
@@ -1,13 +1,16 @@
 import * as WebSocket from 'ws';
+
+// const heartbeat = () {
+//   this.isAlive = true;
+// }
+
 export default class WebSocketContext {
     private static _instance = new WebSocketContext();
     public static getInstance(): WebSocketContext {
       return WebSocketContext._instance;
     }
     private static _websocket;
-    private static _ows: WebSocket.Server;
-    private static _req: any;
-    private static _head: any;
+    private static isAlive: any;
     private constructor() {}
     public getWebsocket(): WebSocket.Server {
       return WebSocketContext._websocket;
@@ -15,9 +18,8 @@ export default class WebSocketContext {
     public static init(websocket) {
       WebSocketContext._websocket = websocket;
       websocket.on('connection', (ws, req, head) => {
-        WebSocketContext._ows = ws;
-        WebSocketContext._req = req;
-        WebSocketContext._head = head;
+        ws.isAlive = true;
+        // ws.on('pong', heartbeat);
         console.log(req.connection.remoteAddress);
         ws.on('open', () => {
             console.log('connected');
@@ -34,4 +36,29 @@ export default class WebSocketContext {
         });
     });
     }
+    // private heartbeat() {
+    //   this.isAlive = true;
+    // }
+    // public static init(websocket) {
+    //   WebSocketContext._websocket = websocket;
+    //   websocket.on('connection', (ws, req, head) => {
+    //     WebSocketContext._ows = ws;
+    //     WebSocketContext._req = req;
+    //     WebSocketContext._head = head;
+    //     console.log(req.connection.remoteAddress);
+    //     ws.on('open', () => {
+    //         console.log('connected');
+    //     });
+    //     ws.on('close', () => {
+    //         console.log('disconnected');
+    //     });
+    //     ws.on('message', (data) => {
+    //         console.log('message', data);
+    //     });
+    //     ws.on('error', (err) => {
+    //         console.log('error', err);
+    //         throw err;
+    //     });
+    // });
+    // }
 }
