832d02ec5b1508f0e8b5231611f7d857ca59ecdf
user <user@arcadiade-MacBook-Pro.local>
2018-08-06 16:15:44 +0800

    0.0.9

--- lib/microServices/WebSocketManager.d.ts
+++ lib/microServices/WebSocketManager.d.ts
@@ -2,5 +2,6 @@ import 'reflect-metadata';
 import * as WebSocket from 'ws';
 export default class WebSocketManager {
     protected _context: WebSocket.Server;
+    protected _ows: WebSocket.Server;
     constructor();
 }
