d3fc0b9e85b732aa073db1a503e66367a273ce03
user <user@arcadiade-MacBook-Pro.local>
2018-08-06 15:03:05 +0800

    0.0.5

--- lib/WSServer.d.ts
+++ lib/WSServer.d.ts
@@ -1,4 +1,3 @@
-import * as WebSocket from 'ws';
 import HttpInitSetting from './models/HttpInitSetting';
 export default class WSServer {
     private main;
@@ -6,5 +5,5 @@ export default class WSServer {
     private koaServer;
     private wssServer;
     constructor(initSetting: HttpInitSetting);
-    start(): Promise<WebSocket.Server>;
+    start(): Promise<void>;
 }
