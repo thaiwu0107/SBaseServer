3d713425c7fd9a4f072953b055e4e0bddb585272
user <user@arcadiade-MacBook-Pro.local>
2018-08-06 15:32:02 +0800

    Initial commit of all files to the repository

--- src/models/WebSocketContext.ts
+++ src/models/WebSocketContext.ts
@@ -1,11 +1,15 @@
 import * as WebSocket from 'ws';
 export default class WebSocketContext {
+    // private static _instance = new WebSocketContext();
+    // private static getInstance(): WebSocketContext {
+    //   return WebSocketContext._instance;
+    // }
     private static _websocket;
     private constructor() {}
     public static getWebsocket(): WebSocket.Server {
-      return this._websocket;
+      return WebSocketContext._websocket;
     }
     public static init(websocket) {
-      this._websocket = websocket;
+      WebSocketContext._websocket = websocket;
     }
 }
