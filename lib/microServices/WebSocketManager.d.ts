2929113c436813aa78634c415ae30f77ceb15df8
user <user@arcadiade-MacBook-Pro.local>
2018-08-07 16:25:47 +0800

    0.0.25

--- lib/microServices/WebSocketManager.d.ts
+++
@@ -1,9 +0,0 @@
-import 'reflect-metadata';
-import * as WebSocket from 'ws';
-export default class WebSocketManager {
-    protected _context: WebSocket.Server;
-    protected _ows: WebSocket.Server;
-    protected _req: any;
-    protected _head: any;
-    constructor();
-}
---
+++ lib/sqlTemplate/QueryDBCurrentTime.sql
@@ -0,0 +1,1 @@
+select SYSDATETIMEOFFSET() AS TS
